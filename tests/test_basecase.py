285e2887e3acb74981e10cf8d786d62b25cce910
BrianLusina <lusinabrian@gmail.com>
2016-11-28 11:03:02 +0300

    add context test case

--- tests/test_basecase.py
+++ tests/test_basecase.py
@@ -5,3 +5,11 @@ from sqlalchemy.exc import IntegrityError
 from app.models import Author, Story
 from app import create_app, db
 
+class ContextTestCase(unittest.TestCase):
+
+    def __call__(self, result=None):
+        try:
+            self._pre_setup()
+            super(ContextTestCase, self).__call__(result)
+        finally:
+            self.
