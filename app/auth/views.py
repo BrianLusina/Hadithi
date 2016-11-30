235d1c214f3f22bcaaf68e643efe5b20f0880e5d
BrianLusina <lusinabrian@gmail.com>
2016-11-30 11:05:38 +0300

    Pass forms to auth module

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from app.forms import LoginForm, RegisterForm
 
 auth = Blueprint(name='auth', url_prefix='/auth', import_name=__name__)
 
 
-@auth.route('')
+@auth.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm(request.form)
+    return render_template('auth/login.html', form=form)
+
+
+@auth.route('/register', methods=["GET", "POST"])
+def register():
+    register_form = RegisterForm(request.form)
+    return render_template('auth/register.html', register_form=register_form)
--- app/forms.py
+++ app/forms.py
@@ -1,16 +1,21 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 
 class LoginForm(Form):
-    email = StringField(label="Email", validators=[DataRequired(), Email()])
+    email = StringField(label="Email/username", validators=[DataRequired(), Email()])
     password = PasswordField(label="Password", validators=[DataRequired()])
 
 
-class SignUpForm(Form):
+class RegisterForm(Form):
     first_name = StringField(label="First Name", validators=[DataRequired()])
     second_name = StringField(label="Last Name", validators=[DataRequired()])
+    username = StringField(label="Username", validators=[DataRequired(), Length(min=5, max=15)])
     email = StringField(label="Email", validators=[DataRequired(), Email()])
-    password = PasswordField(label="Password", validators=[DataRequired()])
+    password = PasswordField(label="Password",
+                             validators=[DataRequired(),
+                                         EqualTo('verify_password', message="Passwords must match"),
+                                         Length(min=8, max=15)
+                                         ])
     verify_password = PasswordField(label="Retype Password", validators=[DataRequired()])
