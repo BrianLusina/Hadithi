21089e7cf68c9df5699e8c4b78085b391c6abccd
BrianLusina <lusinabrian@gmail.com>
2016-11-30 10:43:44 +0300

    add routes for about and contact

--- app/home_page/views.py
+++ app/home_page/views.py
@@ -7,3 +7,15 @@ home_module = Blueprint(name='home', url_prefix='/', import_name=__name__)
 @home_module.route('home')
 def home():
     return render_template('home/index.html')
+
+
+@home_module.route('contact')
+def contact():
+    return render_template('home/contact.html')
+
+
+@home_module.route('about')
+def about():
+    return render_template('home/about.html')
+
+
