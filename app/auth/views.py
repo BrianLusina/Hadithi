abd9ad1693ea0d24d46e4118100b4f19ee2537b9
BrianLusina <lusinabrian@gmail.com>
2016-12-01 08:36:22 +0300

    Add csrf token in register form

--- app/auth/views.py
+++ app/auth/views.py
@@ -16,10 +16,12 @@ def login():
 @auth.route('/register', methods=["GET", "POST"])
 def register():
     register_form = RegisterForm(request.form)
-    if request.method == "POST" and register_form.validate():
-        author = Author(fname=register_form.first_name.data, lname=register_form.second_name.data,
-                        email=register_form.email.data, password=register_form.password.data)
-        db.session.add(author)
-        flash("Thank you for registering")
-        return redirect(url_for('auth.login'))
+    if request.method == "POST":
+        if register_form.validate_on_submit():
+            author = Author(fname=register_form.first_name.data, lname=register_form.second_name.data,
+                            email=register_form.email.data, password=register_form.password.data)
+            db.session.add(author)
+            db.session.commit()
+            flash("Thank you for registering")
+            return redirect(url_for('auth.login'))
     return render_template('auth/register.html', register_form=register_form)
