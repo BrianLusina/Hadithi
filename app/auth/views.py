a189668e3bde4ed9ddd96b4739e3c2948fb8d842
BrianLusina <lusinabrian@gmail.com>
2016-12-01 08:42:01 +0300

    Add login view

--- app/auth/views.py
+++ app/auth/views.py
@@ -9,7 +9,17 @@ auth = Blueprint(name='auth', url_prefix='/auth', import_name=__name__)
 
 @auth.route('/login', methods=["GET", "POST"])
 def login():
+    """
+    Login route for user's to login to their accounts
+    :return: Login view
+    """
     login_form = LoginForm(request.form)
+    if request.method == "POST":
+        if login_form.validate_on_submit():
+            author = Author.query.filter_by(email=login_form.email.data).first()
+            if author is not None and author.verify_password(login_form.password.data):
+                # todo: redirect to author dashboard
+                return redirect(url_for('home.home'))
     return render_template('auth/login.html', login_form=login_form)
 
 
