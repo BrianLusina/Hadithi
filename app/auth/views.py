b094edf6057d348d72c3b0b665f59ab14910c240
BrianLusina <lusinabrian@gmail.com>
2016-12-01 10:57:46 +0300

    update login route

--- app/auth/views.py
+++ app/auth/views.py
@@ -16,21 +16,20 @@ def login():
     login_form = LoginForm(request.form)
     register_form = RegisterForm(request.form)
 
-    if request.method == "POST":
-        if login_form.validate_on_submit():
-            author = Author.query.filter_by(email=login_form.email.data).first()
-            if author is not None and author.verify_password(login_form.password.data):
-                # todo: redirect to author dashboard
-                return redirect(url_for('home.home'))
-            flash('Invalid username or password.', 'error')
-
-        if register_form.validate_on_submit():
-            author = Author(full_name=register_form.full_name.data, email=register_form.email.data,
-                            password=register_form.password.data)
-            db.session.add(author)
-            db.session.commit()
-            flash("Thank you for registering")
+    if request.method == "POST" and login_form.validate_on_submit():
+        author = Author.query.filter_by(email=login_form.email.data).first()
+        if author is not None and author.verify_password(login_form.password.data):
+            # todo: redirect to author dashboard
             return redirect(url_for('home.home'))
+        flash('Invalid username or password.', 'error')
+
+    if request.method == "POST" and register_form.validate_on_submit():
+        author = Author(full_name=register_form.full_name.data, email=register_form.email.data,
+                        password=register_form.password.data)
+        db.session.add(author)
+        db.session.commit()
+        flash("Thank you for registering")
+        return redirect(url_for('home.home'))
     return render_template('auth/auth.html', login_form=login_form, register_form=register_form)
 
 
