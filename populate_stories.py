e7eb545a267e80627db0e2ac1c237168732ac308
BrianLusina <lusinabrian@gmail.com>
2016-12-02 17:29:50 +0300

    add auto populate

--- populate_stories.py
+++ populate_stories.py
@@ -1,10 +1,16 @@
 from app import db
 from app.models import Story, Author
+import json
 
-author = Author(full_name="", email="",password="")
-story = Story(title="", tagline="", category="", content="", author_id=author.id)
+stories = open("stories.json", "r")
+with stories as data:
+    loaded_stories = json.load(data)
 
+author = Author(full_name="Brian M", email="brianombito@email.com", password="password")
 db.session.add(author)
-db.session.add(story)
-db.session.commit()
 
+for l_story in loaded_stories:
+    story = Story(title=l_story.get("title"), tagline=l_story.get("tagline"), category=l_story.get("category"), content=l_story.get("content"), author_id=author.id)
+
+    db.session.add(story)
+db.session.commit()
