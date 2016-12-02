e5d8d5ba5fde01efd78db3b06f8f2a75612888f6
BrianLusina <lusinabrian@gmail.com>
2016-12-02 11:01:22 +0300

    Edit login view

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,30 +1,47 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
+from sqlalchemy.exc import IntegrityError
 from app.forms import LoginForm, RegisterForm, ForgotPassword
 from app.models import Author
 from app import db
 
-
 auth = Blueprint(name='auth', url_prefix='/auth', import_name=__name__)
 
 
-@auth.route('/login', methods=["POST"])
+@auth.route('/login', methods=["POST", "GET"])
 def login():
     """
     Login route for user's to login to their accounts
     :return: Login view
     """
     login_form = LoginForm(request.form, prefix="login_form")
+    register_form = RegisterForm(request.form, prefix="register_form")
 
-    if request.method == "POST" and login_form.login.data:
-        if login_form.validate_on_submit():
+    if request.method == "POST":
+        if request.form["login"] == "LOGIN" and login_form.validate_on_submit():
             author = Author.query.filter_by(email=login_form.email.data).first()
             if author is not None and author.verify_password(login_form.password.data):
                 # todo: redirect to author dashboard
                 return redirect(url_for('home.home'))
-    return render_template('auth/auth.html', login_form=login_form, register_form=RegisterForm())
+            else:
+                # todo: display error
+                return render_template('auth/auth.html')
+
+        if request.form["register"] == "REGISTER" and register_form.validate_on_submit():
+            author = Author(full_name=register_form.full_name.data, email=register_form.email.data,
+                            password=register_form.password.data)
+            try:
+                db.session.add(author)
+                db.session.commit()
+            except IntegrityError as ie:
+                # todo: display error
+                print(ie)
+                db.session.rollback()
+            return redirect(url_for('home.home'))
+
+    return render_template('auth/auth.html', login_form=login_form, register_form=register_form)
 
 
-@auth.route('/register', methods=["POST"])
+@auth.route('/login', methods=["POST", "GET"])
 def register():
     """
     Processes the registration form details. This is used to add the user to the database, if they
@@ -33,13 +50,13 @@ def register():
     """
     register_form = RegisterForm(request.form, prefix="register_form")
 
-    if request.method == "POST" and register_form.register.data:
-        if register_form.validate_on_submit():
-            author = Author(full_name=register_form.full_name.data, email=register_form.email.data,
-                            password=register_form.password.data)
-            db.session.add(author)
-            db.session.commit()
-            return redirect(url_for('home.home'))
+    if request.method == "POST" and (request.form["register"] == "REGISTER" and
+                                     register_form.validate_on_submit()):
+        author = Author(full_name=register_form.full_name.data, email=register_form.email.data,
+                        password=register_form.password.data)
+        db.session.add(author)
+        db.session.commit()
+        return redirect(url_for('home.home'))
 
     return render_template('auth/auth.html', register_form=register_form, login_form=LoginForm())
 
