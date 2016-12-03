b37dd078fa948d3674b3b87e65af2e739ee1b00e
BrianLusina <lusinabrian@gmail.com>
2016-12-03 09:22:29 +0300

    add json files

--- populate_stories.py
+++ populate_stories.py
@@ -3,14 +3,22 @@ from app.models import Story, Author
 import json
 
 stories = open("stories.json", "r")
+authors = open("authors.json", "r")
+
 with stories as data:
     loaded_stories = json.load(data)
 
-author = Author(full_name="Brian M", email="brianombito@email.com", password="password")
-db.session.add(author)
+with authors as data:
+    loaded_authors = json.load(data)
 
 for l_story in loaded_stories:
-    story = Story(title=l_story.get("title"), tagline=l_story.get("tagline"), category=l_story.get("category"), content=l_story.get("content"), author_id=author.id)
-
-    db.session.add(story)
+    for auth in loaded_authors:
+        author = Author(full_name=auth["full_name"], email=auth["email"], password=auth["password"])
+        story = Story(title=l_story.get("title"), tagline=l_story.get("tagline"),
+                      category=l_story.get("category"), content=l_story.get("content"), author_id=author.id)
+        db.session.add(author)
+        db.session.add(story)
 db.session.commit()
+
+
+
