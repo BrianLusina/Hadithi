e5a6d10b0a2af27a5579cfb8e8d8d182908ab90a
BrianLusina <lusinabrian@gmail.com>
2016-12-03 08:47:38 +0300

    Edit provisioning on vagrant

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y apache2
+sudo apt-get update
+sudo apt-get install -y apache2
+
+# Repair "==> default: stdin: is not a tty" message
+sudo sed -i '/tty/!s/mesg n/tty -s \\&\\& mesg n/' /root/.profile
+# Git
+echo 'Installing Git...'
+#sudo apt-get -y install git
+sudo apt-get -y install git > /dev/null 2>&1
 if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant /var/www
