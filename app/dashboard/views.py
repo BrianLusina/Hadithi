419ec16060eef228e55d38b61dffbd342b671433
BrianLusina <lusinabrian@gmail.com>
2016-12-05 10:18:33 +0300

    add route to user dashboard

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import current_user, login_required
 from app.models import Author, Story
+from app.forms import StoryForm
+
+
 dashboard = Blueprint(name="dashboard", url_prefix="/dashboard", import_name=__name__)
 
 
@@ -21,4 +24,5 @@ def write_story():
     Allows Author to write a new story
     :return new story template
     """
-    return render_template("dashboard/new_story.html", user=current_user) 
+    story_form = StoryForm(request.form)
+    return render_template("dashboard/new_story.html", user=current_user, story_form=story_form) 
