0abf8082891abd50337272f771112e09b4a74f51
BrianLusina <lusinabrian@gmail.com>
2017-01-10 09:56:06 +0300

    remove dashboard.css due to conflice

--- ReadMe.md
+++ ReadMe.md
@@ -1,10 +1,10 @@
-# African Short Stories
+# Hadithi
 
 This is a website featuring some of Africa's best short stories titled Hadithi. The site has a sample of 10 short stories and allows the user the ability to share each story on social media. Even make a comment on each story.
 
 Technologies used:
-+ HTML
-+ CSS
++ flask python framework
++ HTML/CSS
 + JavaScript jQuery library
 
 ## setup with Heroku
--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 import os
 from app import create_app, db
 from app.models import Author, Story
-from flask_script import Manager, Shell
+from flask_script import Manager, Shell, Server
 from flask_migrate import MigrateCommand, Migrate
 
 cov = None
@@ -13,6 +13,7 @@ if os.environ.get("FLASK_COVERAGE"):
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
+server = Server(host="127.0.0.1", port=5555)
 
 
 def make_shell_context():
@@ -20,6 +21,7 @@ def make_shell_context():
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
+manager.add_command("runserver", server)
 
 
 @manager.command
