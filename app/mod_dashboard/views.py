1a31371c497a37b97ab589a0c33802428bf74324
BrianLusina <lusinabrian@gmail.com>
2017-01-17 11:16:28 +0300

    add decorators

--- app/mod_dashboard/views.py
+++ app/mod_dashboard/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import current_user, login_required
 from app.models import Author, Story
 from app.forms import StoryForm
+from app.utils.decorators import check_confirmed
 from app import db
 
 dashboard = Blueprint(name="dashboard", url_prefix="/dashboard", import_name=__name__)
@@ -9,9 +10,12 @@ dashboard = Blueprint(name="dashboard", url_prefix="/dashboard", import_name=__n
 
 @dashboard.route("/<string:username>")
 @login_required
+@check_confirmed
 def user_dashboard(username):
     """
     Displays all stories/articles written by this author in a grid
+    Cheks if the user is logged in and logs them into their dashboard,
+    Checks if the user has been confirmed and display the dashboard if they have
     :return user dashboard
     """
     user = current_user
@@ -36,3 +40,8 @@ def write_story():
             db.session.commit()
             return redirect(url_for('dashboard.user_dashboard', user.full_name))
     return render_template("dashboard/new_story.html", user=user, story_form=story_form)
+
+@dashboard.route('/resend')
+@login_required
+def resend_confirmation():
+    
